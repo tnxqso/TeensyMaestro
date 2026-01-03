d0d2e7bcfa75602963c8dae0f5a66de75d52bfb7
tnxqso <250124812+tnxqso-bot@users.noreply.github.com>
2026-01-03 15:11:02 +0100

    v0.9.33

--- firmware/TeensyMaestro/tm_config.h
+++ firmware/TeensyMaestro/tm_config.h
@@ -57,7 +57,7 @@
 #endif
 
 #ifndef TM_VERSION
-  #define TM_VERSION        "0.9.32"     // Semantic version with prerelease stage
+  #define TM_VERSION        "0.9.33"     // Semantic version with prerelease stage
 #endif
 
 #ifndef TM_FULL_NAME
